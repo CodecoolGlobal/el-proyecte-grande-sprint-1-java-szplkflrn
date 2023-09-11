3888d8f8d285e4c51516048dd4a6d5a59eebe591
szplkflrn <119355053+szplkflrn@users.noreply.github.com>
2023-09-11 14:23:17 +0200

    build(Whole Project): add starting structure

---
+++ src/main/java/com/codecool/bytebattlers/ByteBattlersApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.bytebattlers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteBattlersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteBattlersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/bytebattlers/ByteBattlersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.bytebattlers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteBattlersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
