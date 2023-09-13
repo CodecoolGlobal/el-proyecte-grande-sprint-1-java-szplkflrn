48d5552a920148217358b7cad8b402a972aeecda
marktamasko <25462245+marktamasko@users.noreply.github.com>
2023-09-13 11:18:11 +0200

    feat[BoardGameStorageTest]: create BoardGameStorageTest & test addBoardGame method

---
+++ backend/src/test/java/com/codecool/bytebattlers/service/storage/BoardGameStorageTest.java
@@ -0,0 +1,57 @@
+package com.codecool.bytebattlers.service.storage;
+
+import com.codecool.bytebattlers.dao.model.BoardGame;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardGameStorageTest {
+
+    private BoardGameStorage storage;
+
+    @BeforeEach
+    void setUp() {
+        this.storage = new BoardGameStorage();
+    }
+
+    @Test
+    void test_addBoardGame_ReturnCorrectSize() {
+        // Arrange
+        int expected = 2;
+        BoardGame game1 = new BoardGame(
+                1, "A", 1, 2, 20,
+                10, "aa", 2, 1, 1);
+        BoardGame game2 = new BoardGame(
+                2, "B", 2, 4, 300,
+                18, "bb", 5, 2, 2);
+
+        // Act
+        this.storage.addBoardGame(game1);
+        this.storage.addBoardGame(game2);
+        int actual = this.storage.getAllGame().size();
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void test_addBoardGame_addSameGame() {
+        // Arrange
+        int expected = 1;
+        BoardGame game1 = new BoardGame(
+                1, "A", 1, 2, 20,
+                10, "aa", 2, 1, 1);
+        BoardGame game2 = new BoardGame(
+                1, "A", 1, 2, 20,
+                10, "aa", 2, 1, 1);
+
+        // Act
+        this.storage.addBoardGame(game1);
+        this.storage.addBoardGame(game2);
+        int actual = this.storage.getAllGame().size();
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+}
