886b8f23e3563b833feb79db065b324ae0b803aa
IPHUN1989 <tamas.peter.illes@gmail.com>
2023-09-14 11:20:48 +0200

    ✨ feat(main.jsx): Add paths to main
    Add paths for React Router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Layout from './Pages/Layout/Layout.jsx';
+import HomePage from './Pages/HomePage/HomePage.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import GameList from './Pages/GameList.jsx';
+import BoardGameCreator from './Pages/BoardGameCreator.jsx';
+import ErrorPage from './Pages/ErrorPage.jsx';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/games",
+        element: <GameList />,
+      },
+      {
+        path: "/games/create",
+        element: <BoardGameCreator />,
+      }
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
