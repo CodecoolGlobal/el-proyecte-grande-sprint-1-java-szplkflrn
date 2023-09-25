5d795e991ca7d74bacc3d6f6cfb35b657ab4937f
marktamasko <25462245+marktamasko@users.noreply.github.com>
2023-09-25 14:14:38 +0200

    feat[UserEntity]: create UserEntity with JPA Buddy

---
+++ backend/src/main/java/com/codecool/bytebattlers/model/UserEntity.java
@@ -0,0 +1,28 @@
+package com.codecool.bytebattlers.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user_entity")
+public class UserEntity {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+}
