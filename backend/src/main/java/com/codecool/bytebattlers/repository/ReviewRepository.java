4f5346e3bcccdb9d8021d3e22965901a90e12b24
szplkflrn <119355053+szplkflrn@users.noreply.github.com>
2023-09-26 12:26:18 +0200

    feat(ReviewRepository): create ReviewRepository interface

---
+++ backend/src/main/java/com/codecool/bytebattlers/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.codecool.bytebattlers.repository;
+
+import com.codecool.bytebattlers.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
