9daad9414277a863fb0b10b5171b93c03098b762
IPHUN1989 <tamas.peter.illes@gmail.com>
2023-09-26 15:25:04 +0200

    feat(Category): Add Category class
    Add Category class

--- backend/src/main/java/com/codecool/bytebattlers/model/Category.java
+++ backend/src/main/java/com/codecool/bytebattlers/model/Category.java
@@ -1,4 +1,57 @@
 package com.codecool.bytebattlers.model;
 
-public record Category(Integer id, String categoryName, String description) {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.proxy.HibernateProxy;
+import org.hibernate.type.SqlTypes;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "public_id", nullable = false, unique = true)
+    @JdbcTypeCode(SqlTypes.UUID)
+    private UUID publicID = UUID.randomUUID();
+
+    @Column(name = "name", nullable = false, unique = true, length = 50)
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    private String name;
+
+    @Lob
+    @Column(name = "description", nullable = false)
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    private String description;
+
+
+    @ManyToMany(mappedBy = "categories", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH})
+    private Set<BoardGame> boardGames = new LinkedHashSet<>();
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Category category = (Category) o;
+        return getId() != null && Objects.equals(getId(), category.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
