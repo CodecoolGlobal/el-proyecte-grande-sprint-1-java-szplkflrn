62768145f89ebc235ea800037db21fda07856a42
IPHUN1989 <tamas.peter.illes@gmail.com>
2023-09-26 18:26:39 +0200

    fix(Repositories): Adding @Repository annotation
    Adding @Repository annotation

--- backend/src/main/java/com/codecool/bytebattlers/repository/PublisherRepository.java
+++ backend/src/main/java/com/codecool/bytebattlers/repository/PublisherRepository.java
@@ -2,6 +2,8 @@ package com.codecool.bytebattlers.repository;
 
 import com.codecool.bytebattlers.model.Publisher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PublisherRepository extends JpaRepository<Publisher, Long> {
 }
