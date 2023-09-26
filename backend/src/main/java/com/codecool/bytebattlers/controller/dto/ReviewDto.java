190c99bb51c1352c8b921f28bfd07a2420dbbcd1
IPHUN1989 <tamas.peter.illes@gmail.com>
2023-09-26 15:15:52 +0200

    refactor(ReviewDto): Refactor ReviewDto record
    Refactor ReviewDto record with necessary associations

--- backend/src/main/java/com/codecool/bytebattlers/controller/dto/ReviewDto.java
+++ backend/src/main/java/com/codecool/bytebattlers/controller/dto/ReviewDto.java
@@ -1,10 +1,11 @@
 package com.codecool.bytebattlers.controller.dto;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 /**
  * DTO for {@link com.codecool.bytebattlers.model.Review}
  */
-public record ReviewDto(String description, Long boardGameId, String boardGameGameName, Long userId,
-                        String userUserName) implements Serializable {
+public record ReviewDto(UUID publicID, String description, UUID boardGamePublicID, String boardGameGameName,
+                        UUID userPublicID, String userUserName) implements Serializable {
 }
