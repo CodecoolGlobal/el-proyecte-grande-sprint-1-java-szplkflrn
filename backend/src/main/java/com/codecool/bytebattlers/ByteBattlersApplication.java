ea7cca2c9f14cc0d7007bc11650f311afea32fc5
IPHUN1989 <tamas.peter.illes@gmail.com>
2023-09-26 18:33:53 +0200

    fix(ByteBattlersApplication): Adding a try-catch to main
    Adding a try-catch to main

--- backend/src/main/java/com/codecool/bytebattlers/ByteBattlersApplication.java
+++ backend/src/main/java/com/codecool/bytebattlers/ByteBattlersApplication.java
@@ -1,13 +1,22 @@
 package com.codecool.bytebattlers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ByteBattlersApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ByteBattlersApplication.class, args);
-	}
+	private static final Logger logger = LoggerFactory.getLogger(ByteBattlersApplication.class);
 
+
+	public static void main(String[] args) {
+
+		try {
+			SpringApplication.run(ByteBattlersApplication.class, args);
+		} catch (Exception e) {
+		logger.error("An error occurred during application startup\", e");
+		}
+	}
 }
