8d8acf0922072b1548133d9f090ced7ca52bbcfa
IPHUN1989 <tamas.peter.illes@gmail.com>
2023-09-26 15:10:33 +0200

    feat(CategoryRepository): Add CategoryRepository interface
    Add CategoryRepository interface

---
+++ backend/src/main/java/com/codecool/bytebattlers/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.codecool.bytebattlers.repository;
+
+import com.codecool.bytebattlers.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
