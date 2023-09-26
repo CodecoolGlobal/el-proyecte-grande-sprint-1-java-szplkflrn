4e2e7f40e322081822e0753ed45f5cdf4c6d9420
IPHUN1989 <tamas.peter.illes@gmail.com>
2023-09-26 18:28:14 +0200

    fix(Controllers): Unifying the endpoint naming conventions to plurals
    Unifying the endpoint naming conventions to plurals

--- backend/src/main/java/com/codecool/bytebattlers/controller/PublisherController.java
+++ backend/src/main/java/com/codecool/bytebattlers/controller/PublisherController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/publisher")
+@RequestMapping("/api/publishers")
 public class PublisherController {
 
     private final PublisherService service;
--- backend/src/main/java/com/codecool/bytebattlers/controller/ReviewController.java
+++ backend/src/main/java/com/codecool/bytebattlers/controller/ReviewController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/review")
+@RequestMapping("/api/reviews")
 public class ReviewController {
 
 
