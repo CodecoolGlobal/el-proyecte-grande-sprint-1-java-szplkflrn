a8dc29f17497742ae5eb71b534afc62bf2cfa965
IPHUN1989 <tamas.peter.illes@gmail.com>
2023-09-28 00:13:13 +0200

    feat(AppUserRepository): Add new method to find a user by public id
    Add new method to find a user by public id

--- backend/src/main/java/com/codecool/bytebattlers/repository/AppUserRepository.java
+++ backend/src/main/java/com/codecool/bytebattlers/repository/AppUserRepository.java
@@ -3,5 +3,9 @@ package com.codecool.bytebattlers.repository;
 import com.codecool.bytebattlers.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.UUID;
+
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
+    AppUser findAppUsersByPublicID(UUID publicID);
 }
