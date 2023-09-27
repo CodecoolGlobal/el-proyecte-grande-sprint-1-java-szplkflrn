10ad982d35b7291cc4a5e3fa995c5bba99419dc5
IPHUN1989 <tamas.peter.illes@gmail.com>
2023-09-27 22:32:45 +0200

    fix(User related classes): Delete user related classes
    Delete user related classes

--- backend/src/main/java/com/codecool/bytebattlers/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codecool.bytebattlers.repository;
-
-import com.codecool.bytebattlers.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
