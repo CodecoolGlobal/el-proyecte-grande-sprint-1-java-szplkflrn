36a235902471ce21f06a51b1ea95113b697414e6
IPHUN1989 <tamas.peter.illes@gmail.com>
2023-09-28 16:34:57 +0200

    refactor(BoardGameController): Refactor BoardGameController to send response entity
    Refactor BoardGameController to send response entity

--- backend/src/main/java/com/codecool/bytebattlers/controller/BoardGameController.java
+++ backend/src/main/java/com/codecool/bytebattlers/controller/BoardGameController.java
@@ -1,12 +1,16 @@
 package com.codecool.bytebattlers.controller;
 import com.codecool.bytebattlers.controller.dto.BoardGameDto;
+import com.codecool.bytebattlers.controller.exception.ResourceNotFoundException;
 import com.codecool.bytebattlers.service.BoardGameService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/games")
@@ -23,27 +27,31 @@ public class BoardGameController {
     }
 
     @GetMapping
-    public List<BoardGameDto> getAllBoardGame() {
-        return boardGameService.findAll();
-    }
-
-    @GetMapping("/{id}")
-    public BoardGameDto getBoardGameById(@PathVariable Long id) {
-       return boardGameService.findById(id);
-    }
-
-    @PostMapping
-    public void addNewBoardGame(@RequestBody BoardGameDto board) {
-        try {
-            boardGameService.save(board);        }
-        catch (Exception e) {
-            logger.error("An error occurred during application startup", e);
+    public ResponseEntity<List<BoardGameDto>> getAllCategory() {
+        if (boardGameService.findAll().isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(boardGameService.findAll(), HttpStatus.OK);
         }
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<BoardGameDto> getCategoryById(@PathVariable UUID id) {
+        if (boardGameService.findByPublicID(id) == null) {
+            throw new ResourceNotFoundException("Not found Tutorial with id = " + id);
+        } else {
+            return new ResponseEntity<>(boardGameService.findByPublicIdToDTO(id), HttpStatus.OK);
+        }
+    }
+    @PostMapping
+    public ResponseEntity<BoardGameDto>  addNewCategory(@RequestBody BoardGameDto board) {
+        return new ResponseEntity<>(boardGameService.save(board), HttpStatus.CREATED);
+    }
+
     @DeleteMapping("/{id}")
-    public void deleteBoardGameById(@PathVariable Long id) {
-         boardGameService.deleteById(id);
+    public ResponseEntity<BoardGameDto> deleteCategoryById(@PathVariable UUID id) {
+        boardGameService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
