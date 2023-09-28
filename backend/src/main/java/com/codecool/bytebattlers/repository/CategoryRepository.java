d102694f2c7e399374bd65429e24827d6af15b00
IPHUN1989 <tamas.peter.illes@gmail.com>
2023-09-28 14:41:46 +0200

    feat(CategoryRepository): Add delete and findbyPublicID methods
    Add delete and findbyPublicID methods

--- backend/src/main/java/com/codecool/bytebattlers/repository/CategoryRepository.java
+++ backend/src/main/java/com/codecool/bytebattlers/repository/CategoryRepository.java
@@ -4,6 +4,13 @@ import com.codecool.bytebattlers.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Category findCategoryByPublicID(UUID publicID);
+
+    void deleteByPublicID(UUID publicID);
+
 }
