6a6f25692ca78a26686faf9ac22f6a0c13868825
szplkflrn <119355053+szplkflrn@users.noreply.github.com>
2023-09-28 10:49:47 +0200

    refactor(GameList): refactor GameList to show Games from Database

--- frontend/src/Pages/GameList.jsx
+++ frontend/src/Pages/GameList.jsx
@@ -1,5 +1,4 @@
-import * as React from 'react';
-import { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
@@ -7,7 +6,7 @@ import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
-import { TableHead } from '@mui/material';
+import TableHead from '@mui/material/TableHead';
 import TableFooter from '@mui/material/TableFooter';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
@@ -79,10 +78,6 @@ TablePaginationActions.propTypes = {
   rowsPerPage: PropTypes.number.isRequired,
 };
 
-
-
-
-
 export default function GameList() {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
@@ -102,20 +97,15 @@ export default function GameList() {
     gamesFetch();
   }, []);
 
-
-  function createData(name, description, rating) {
-    return { name, description, rating };
+  function createData(name, minplayer, maxplayer, rating) {
+    return { name, minplayer, maxplayer, rating };
   }
-  
+
   const rows = games.map((game) => {
-    return createData(game.gameName, game.description, game.rating);
+    return createData(game.gameName, game.minPlayer, game.maxPlayer, game.rating);
   });
 
-  
-
-  // Avoid a layout jump when reaching the last page with empty rows.
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -126,66 +116,63 @@ export default function GameList() {
     setPage(0);
   };
 
+  const startIndex = page * rowsPerPage;
+  const endIndex = startIndex + rowsPerPage;
+  const displayedRows = rows.slice(startIndex, endIndex);
+
   return (
     <div>
-    <br></br>
-    <br></br>
-    <TableContainer component={Paper} sx={{ maxWidth: 800, margin: 'auto' }} align="center" >
-      <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
-      <TableHead>
-          <TableRow>
-            <TableCell>Board Game</TableCell>
-            <TableCell align="center">Description</TableCell>
-            <TableCell align="center">Rating</TableCell>
-          
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {(rowsPerPage > 0
-            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            : rows
-          ).map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row" style={{ width: 160 }}>
-                {row.name}
-              </TableCell>
-              <TableCell style={{ width: 160 }} align="center">
-                {row.description}
-              </TableCell>
-              <TableCell style={{ width: 160 }} align="center">
-                {row.rating}
-              </TableCell>
+      <br />
+      <br />
+      <TableContainer component={Paper} sx={{ maxWidth: 1200, margin: 'auto' }} align="center">
+        <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Board Game</TableCell>
+              <TableCell align="center">Min Player</TableCell>
+              <TableCell align="center">Max Player</TableCell>
+              <TableCell align="center">Rating</TableCell>
             </TableRow>
-          ))}
-          {emptyRows > 0 && (
-            <TableRow style={{ height: 53 * emptyRows }}>
-              <TableCell colSpan={6} />
+          </TableHead>
+          <TableBody>
+            {displayedRows.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row" style={{ width: 160 }}>
+                  {row.name}
+                </TableCell>
+                <TableCell component="th" scope="row" style={{ width: 160 }}>
+                  {row.minplayer}
+                </TableCell>
+                <TableCell component="th" scope="row" style={{ width: 160 }}>
+                  {row.maxplayer}
+                </TableCell>
+                <TableCell style={{ width: 160 }} align="center">
+                  {row.rating}
+                </TableCell>
+              </TableRow>
+            ))}
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}>
+                <TableCell colSpan={4} />
+              </TableRow>
+            )}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                colSpan={4}
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
             </TableRow>
-          )}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TablePagination
-              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-              colSpan={3}
-              count={rows.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              SelectProps={{
-                inputProps: {
-                  'aria-label': 'rows per page',
-                },
-                native: true,
-              }}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-              ActionsComponent={TablePaginationActions}
-            />
-          </TableRow>
-        </TableFooter>
-      </Table>
-    </TableContainer>
+          </TableFooter>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
-
