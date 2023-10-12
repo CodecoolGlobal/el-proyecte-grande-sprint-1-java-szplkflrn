8e51d655a0e7d92e6d5f190b8750b006ee6da00e
IPHUN1989 <tamas.peter.illes@gmail.com>
2023-10-12 23:41:37 +0200

    ✨ feat(BoardGameRepository): Add new queries
    Add new query methods to filter with multiple options

--- backend/src/main/java/com/codecool/bytebattlers/repository/BoardGameRepository.java
+++ backend/src/main/java/com/codecool/bytebattlers/repository/BoardGameRepository.java
@@ -1,6 +1,8 @@
 package com.codecool.bytebattlers.repository;
 
 import com.codecool.bytebattlers.model.BoardGame;
+import com.codecool.bytebattlers.model.Category;
+import com.codecool.bytebattlers.model.Publisher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -18,5 +20,21 @@ public interface BoardGameRepository extends JpaRepository<BoardGame, Long> {
     void deleteByPublicID(UUID publicID);
 
     List<BoardGame> findBoardGamesByGameNameContainingIgnoreCase(String boardGameName);
+
+    List<BoardGame> findBoardGamesByPublisher(Publisher publicID);
+
+    List<BoardGame> findBoardGamesByCategoriesContaining(Category publicID);
+
+    List<BoardGame> findBoardGamesByDescriptionContainsIgnoreCase(String description);
+
+    List<BoardGame> findAllByOrderByGameNameAsc();
+
+    List<BoardGame> findAllByOrderByGameNameDesc();
+
+    List<BoardGame> findAllByMaxPlayerLessThanEqual(int max);
+
+    List<BoardGame> findAllByMinPlayerGreaterThanEqual(int min);
+
+    List<BoardGame> findBoardGamesByRatingGreaterThanEqual(double min);
 
 }
