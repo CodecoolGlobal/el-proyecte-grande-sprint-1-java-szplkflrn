509a7be7c69b9cefd2dcf7dcecb6e3fae89efcfc
IPHUN1989 <tamas.peter.illes@gmail.com>
2023-10-16 15:46:14 +0200

    fix(AppUserRepository): Add missing Repository annotation
    Add missing Repository annotation

--- backend/src/main/java/com/codecool/bytebattlers/repository/AppUserRepository.java
+++ backend/src/main/java/com/codecool/bytebattlers/repository/AppUserRepository.java
@@ -1,12 +1,13 @@
 package com.codecool.bytebattlers.repository;
 
 import com.codecool.bytebattlers.model.AppUser;
-import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
 
     AppUser findAppUsersByPublicID(UUID publicID);
