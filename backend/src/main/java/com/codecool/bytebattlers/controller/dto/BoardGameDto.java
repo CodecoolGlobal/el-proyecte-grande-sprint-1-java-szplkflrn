dd0640e6f98da60309606e7ce959dac47196d98c
szplkflrn <119355053+szplkflrn@users.noreply.github.com>
2023-10-26 13:14:01 +0200

    feat(BoardGameDto record): add new field to BoardGameDto class

--- backend/src/main/java/com/codecool/bytebattlers/controller/dto/BoardGameDto.java
+++ backend/src/main/java/com/codecool/bytebattlers/controller/dto/BoardGameDto.java
@@ -9,8 +9,8 @@ import java.util.UUID;
  */
 public record BoardGameDto(UUID publicID, String gameName, int minPlayer, int maxPlayer, int playTimeInMinutes,
                            Set<CategoryDto1> categories, int recommendedAge, String description, double rating,
-                           UUID publisherPublicID, String publisherPublisherName,
-                           Set<ReviewDto1> reviews) implements Serializable {
+                           UUID publisherPublicID, String publisherPublisherName, Set<ReviewDto1> reviews,
+                           Set<UUID> appUserPublicIDS) implements Serializable {
     /**
      * DTO for {@link com.codecool.bytebattlers.model.Category}
      */
