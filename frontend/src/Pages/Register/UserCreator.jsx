111c3dc60638c711eac0d41a25f0dc2bca9ad251
szplkflrn <119355053+szplkflrn@users.noreply.github.com>
2023-10-26 13:02:25 +0200

    feat(UserCreator): add UserCreator component

---
+++ frontend/src/Pages/Register/UserCreator.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+import Register from "./Register";
+import { useNavigate } from "react-router-dom";
+
+const UserCreator = () => {
+
+    const [users, setUsers] = useState([]);
+    const navigate = useNavigate();
+
+
+    const createUser = async (user) => {
+        const res = await fetch("/api/users/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+        });
+
+        return await res.text();
+    }
+
+
+    const userFetch = async () => {
+        try {
+            const response = await fetch("/api/users");
+            const data = await response.json();
+            setUsers(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        userFetch();
+    }, []);
+
+    const handleCreateUser = (user) => {
+        let userExists = false;
+        for (let i = 0; i < users.length; i++) {
+            if (user.name === users[i].name) {
+                userExists = true;
+                break;
+            }
+        }
+        if (userExists) {
+            alert("This user is already registered!");
+        } else {
+            createUser(user)
+                .then(() => {
+                    navigate("/");
+                })
+                .catch((error) => {
+                    console.error("Error creating user: ", error);
+                })
+        }
+    };
+    return (
+        <Register
+            onCancel={() => navigate("/")}
+            onSave={handleCreateUser}
+        />
+    );
+}
+
+export default UserCreator;
