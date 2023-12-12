cba6540ba9f5bfb63ec93bf4b4cff4d752c7d783
IPHUN1989 <tamas.peter.illes@gmail.com>
2023-12-12 13:25:10 +0100

    feat(RatingRepository): Add RatingRepository interface
    Add RatingRepository interface

---
+++ backend/src/main/java/com/codecool/bytebattlers/repository/RatingRepository.java
@@ -0,0 +1,15 @@
+package com.codecool.bytebattlers.repository;
+
+import com.codecool.bytebattlers.model.Rating;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface RatingRepository extends JpaRepository<Rating, Long> {
+
+    List<Rating> findAllByBoardGame_PublicID(UUID boardGameId);
+
+}
