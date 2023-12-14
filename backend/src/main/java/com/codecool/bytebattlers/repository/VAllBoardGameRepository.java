63fa03c7711318de083168ad18bd893c2ea200f8
IPHUN1989 <tamas.peter.illes@gmail.com>
2023-12-14 13:44:04 +0100

    feat(VAllBoardGameRepository): Add advanced filter methods to the view
    Add advanced filter methods to the view

--- backend/src/main/java/com/codecool/bytebattlers/repository/VAllBoardGameRepository.java
+++ backend/src/main/java/com/codecool/bytebattlers/repository/VAllBoardGameRepository.java
@@ -3,7 +3,28 @@ package com.codecool.bytebattlers.repository;
 import com.codecool.bytebattlers.model.VAllBoardGame;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface VAllBoardGameRepository extends JpaRepository<VAllBoardGame, UUID> {
+
+    List<VAllBoardGame> findBoardGamesByGameNameContainingIgnoreCase(String boardGameName);
+
+    List<VAllBoardGame> findVAllBoardGamesByPublisherName(String publisherName);
+
+    List<VAllBoardGame> findVAllBoardGamesByCategoriesContaining(String category);
+
+    List<VAllBoardGame> findVAllBoardGamesByDescriptionContainingIgnoreCase(String description);
+
+    List<VAllBoardGame> findAllByOrderByGameNameAsc();
+
+    List<VAllBoardGame> findAllByOrderByGameNameDesc();
+
+    List<VAllBoardGame> findAllByMaxPlayerLessThanEqual(int max);
+
+    List<VAllBoardGame> findAllByMinPlayerGreaterThanEqual(int min);
+
+    List<VAllBoardGame> findAllByAverageRatingGreaterThanEqual(double rating);
+
+
 }
