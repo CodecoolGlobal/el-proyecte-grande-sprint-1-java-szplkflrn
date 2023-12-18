8a05320d51fe3ccf588e5f02d807fdcaa5ec72b7
IPHUN1989 <tamas.peter.illes@gmail.com>
2023-12-18 19:15:29 +0100

    feat(RatingController): Add a new enpoint to check if a rating for a specific board game and user combo exist already
    Add a new enpoint to check if a rating for a specific board game and
    user combo exist already

--- backend/src/main/java/com/codecool/bytebattlers/controller/RatingController.java
+++ backend/src/main/java/com/codecool/bytebattlers/controller/RatingController.java
@@ -1,6 +1,7 @@
 package com.codecool.bytebattlers.controller;
 
 import com.codecool.bytebattlers.controller.dto.RatingDto;
+import com.codecool.bytebattlers.mapper.RatingMapper;
 import com.codecool.bytebattlers.service.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,16 +9,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/ratings")
 public class RatingController {
 
     private final RatingService ratingService;
+    private final RatingMapper ratingMapper;
 
     @Autowired
-    public RatingController(RatingService ratingService) {
+    public RatingController(RatingService ratingService,
+                            RatingMapper ratingMapper) {
         this.ratingService = ratingService;
+        this.ratingMapper = ratingMapper;
     }
 
     @GetMapping
@@ -26,6 +31,16 @@ public class RatingController {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } else {
             return new ResponseEntity<>(ratingService.findAll(), HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/check-existence")
+    public ResponseEntity<RatingDto> checkRating(@RequestParam(name = "appUserPublicID") UUID appUserPublicID,
+                                                 @RequestParam(name = "boardGamePublicID") UUID boardGamePublicID) {
+        if (ratingService.checkIfRatingExist(appUserPublicID, boardGamePublicID) == null) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(ratingMapper.toDto(ratingService.checkIfRatingExist(appUserPublicID, boardGamePublicID)), HttpStatus.OK);
         }
     }
 
